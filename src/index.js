1c87a9c6144974f7412e7e4d35c409b8bf9bc5cd
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-03-27 03:32:22 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import Notiflix, { Loading } from 'notiflix';
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import CardsApiPixabay from './js/fetchCards';
 
-import fetchCards from './js/fetchCards';
-
+// =================== REFS
 const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('input'),
@@ -14,35 +14,62 @@ const refs = {
 };
 
 const { form, input, submitBtn, loadMoreBtn, galleryBox } = refs;
-//
+
+const cardsApiPixabay = new CardsApiPixabay();
 
 form.addEventListener('submit', onSearchForm);
-//
-//    LIGHTBOX
-//
-//
-let gallery = new SimpleLightbox('.gallery a');
-//
-//
-function onSearchForm(e) {
-  //   console.log(e.currentTarget.elements.searchQuery.value);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+}); //    LIGHTBOX
+
+//gallery__scroll
+// Скрытая кнопка
+
+const hideLoadMoreBtn = () => (refs.loadMoreBtn.style.display = 'none');
+const showLoadMoreBtn = () => (
+  (refs.loadMoreBtn.style.display = 'block'),
+  loadMoreBtn.addEventListener('click', onLoadMore)
+);
+hideLoadMoreBtn();
+
+async function onSearchForm(e) {
   e.preventDefault();
+  clearCardsGallery();
+  cardsApiPixabay.query = e.currentTarget.elements.searchQuery.value.trim(); // ========== метод trim();
+  cardsApiPixabay.resetPage();
 
-  const searchQuery = e.currentTarget.elements.searchQuery.value;
+  try {
+    if (cardsApiPixabay.query === '') {
+      hideLoadMoreBtn();
+      // ======= нет запроса на путую строку
+      Notiflix.Notify.failure('Please, fill in the field');
+      return;
+    }
 
-  fetchCards(searchQuery)
-    .then(cards => {
-      console.log(cards);
-      renderCards(cards);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    const cards = await cardsApiPixabay.fetchCards();
+    renderCards(cards);
+
+    if (cards.data.hits.length === 0) {
+      hideLoadMoreBtn();
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    Notiflix.Notify.success(`Hooray! We found ${cards.data.totalHits} images.`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 function renderCards(cards) {
-  console.log(cards.hits);
-  const markup = cards.hits
+  console.log(cards.data);
+  showLoadMoreBtn();
+  const markup = cards.data.hits
     .map(
       ({
         webformatURL,
@@ -85,4 +112,40 @@ function renderCards(cards) {
     .join('');
   galleryBox.insertAdjacentHTML('beforeend', markup);
   gallery.refresh();
+}
+
+async function onLoadMore(e) {
+  cardsApiPixabay.incrementPage();
+  try {
+    const cards = await cardsApiPixabay.fetchCards();
+    renderCards(cards);
+
+    // console.log(cards.data.totalHits);
+    const totalPages = Math.ceil(
+      cards.data.totalHits / cardsApiPixabay.perPage
+    );
+    // console.log(totalPages);
+    if (totalPages < cardsApiPixabay.page) {
+      hideLoadMoreBtn();
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function clearCardsGallery() {
+  galleryBox.innerHTML = '';
+  cardsApiPixabay.resetPage();
 }
--- src/js/fetchCards.js
+++ src/js/fetchCards.js
@@ -1,14 +1,39 @@
+import axios from 'axios';
+
 const API_KEY = '34559204-ec632097002210846bac1ec49';
 const BASE_URL = 'https://pixabay.com/api/';
 const params = '&image_type=photo&orientation=horizontal&safesearch=true';
 
-export default function fetchCards(searchQuery) {
-  return fetch(`${BASE_URL}/?key=${API_KEY}&q=${searchQuery}${params}`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+export default class CardsApiPixabay {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 40;
+  }
+
+  async fetchCards() {
+    // console.log(this);
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}${params}&per_page=${this.perPage}&page=${this.page}`
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
     }
-  );
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
